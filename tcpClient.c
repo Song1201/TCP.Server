4c1a4ba9b40d13c8a35541c8adb8959bb15cff0c
Song1201 <songchenli1201@outlook.com>
2019-01-11 18:34:22 -0800

    tcpClient now can take server ip address and port number as input

--- tcpClient.c
+++ tcpClient.c
@@ -1,22 +1,40 @@
 #include <netdb.h> // To use sockaddr
 #include "common.h" // Message structure
-#include <stdio.h> // To use printf
+#include <stdio.h> // To use printf()
+#include <arpa/inet.h> // To use inet_pton()
+#include <string.h> // To use strlen()
+#include <stdlib.h> // To use exit()
 
-#define DEST_PORT 2000
-#define SERVER_IP_ADDRESS "127.0.0.1" 
+#define IPV4_ADDR_MAX_LEN 15
 
 testStructType clientData;
 resultStructType result;
 
-void setupTcpCommunication() {
-  int sockFd = 0, sentRecvBytes = 0;
-  int addrLen = sizeof(struct sockaddr);
-  struct sockaddr_in dest;
-  dest.sin_family = AF_INET;
+struct sockaddr_in setupDestIp() {
+  // sockaddr_in means destination(server) address is IPv4 address
+  struct sockaddr_in dest; 
+  dest.sin_family = AF_INET; // Address Family is IPv4 address
+  printf("Please input server IPv4 address: ");
+  char SERVER_IPV4_ADDR[IPV4_ADDR_MAX_LEN+1];
+  fgets(SERVER_IPV4_ADDR,sizeof(SERVER_IPV4_ADDR),stdin);
+  // Remove the '\n' from input string.
+  SERVER_IPV4_ADDR[strlen(SERVER_IPV4_ADDR)-1] = '\0';  
+  if (inet_pton(AF_INET,SERVER_IPV4_ADDR, &(dest.sin_addr)) != 1) {
+    printf("Failed to read server IPv4 address. Exiting program.\n");
+    exit(1);
+  }
+  printf("Please input server port: ");
+  unsigned short DEST_PORT;
+  scanf("%hd",&DEST_PORT);
   dest.sin_port = DEST_PORT;
-  struct hostent *host = (struct hostent*)gethostbyname(SERVER_IP_ADDRESS);
-  dest.sin_addr = *((struct in_addr*)host->h_addr_list[0]);
-  sockFd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  return dest;
+}
+
+void setupTcpCommunication() {
+  struct sockaddr_in dest = setupDestIp();
+
+  int sockFd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  int addrLen = sizeof(struct sockaddr);
   connect(sockFd,(struct sockaddr*)&dest,addrLen);
   while (1) {
     printf("Enter a: ");
@@ -24,7 +42,7 @@ void setupTcpCommunication() {
     printf("Enter b: ");
     scanf("%u",&clientData.b);
 
-    sentRecvBytes = sendto(sockFd,&clientData,sizeof(testStructType),0,
+    int sentRecvBytes = sendto(sockFd,&clientData,sizeof(testStructType),0,
       (struct sockaddr*)&dest,addrLen);
     printf("Number of bytes sent = %d\n",sentRecvBytes);
     sentRecvBytes = recvfrom(sockFd,(char*)&result,sizeof(resultStructType),0,
